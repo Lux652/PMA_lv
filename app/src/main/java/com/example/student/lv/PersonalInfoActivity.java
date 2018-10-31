c4c056573613c652d6483b23d4c8b9be79c169d6
Lux652 <lukakovacicster@gmail.com>
2018-10-31 18:56:18 +0100

    +studentInfoActivity

--- app/src/main/java/com/example/student/lv/PersonalInfoActivity.java
+++ app/src/main/java/com/example/student/lv/PersonalInfoActivity.java
@@ -1,13 +1,30 @@
 package com.example.student.lv;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PersonalInfoActivity extends AppCompatActivity {
-
+private Button oBtnUpisi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_info);
+        oBtnUpisi = (Button)findViewById(R.id.buttonSend);
+        oBtnUpisi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PersonalInfoActivity.this, StudentInfoActivity.class));
+            }
+        });
+
+
+
+
+
+
+
     }
 }
